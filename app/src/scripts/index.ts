781fa166b714fbb110c86f0310ba2634daefe17b
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-15 12:41:02 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -9,3 +9,19 @@ tl.fromTo(elon, 0.8, { transform: 'scale(0)', opacity: '0' }, { transform: 'scal
   .fromTo(elips, 1.2, { transform: 'scale(0)', opacity: '0' }, { transform: 'scale(1)', opacity: '1', ease: Power2.easeInOut })
   .fromTo(title, 1.2, { opacity: '0', transform: 'translateX(100%)' }, { opacity: '1', transform: 'translateX(0)', ease: Power2.easeInOut }, '-=1.2')
   .fromTo(btn, 0.3, { opacity: '0' }, { opacity: '1', ease: Power2.easeInOut })
+
+
+
+
+  
+  document.addEventListener("mousemove", parallax);
+  function parallax(e){
+    document.querySelectorAll(".parallax").forEach(function(move){
+
+      var moving_value = move.getAttribute("data-value");
+      var x = (e.clientX * moving_value) / 250;
+      var y = (e.clientY * moving_value) / 250;
+
+      move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+  }
